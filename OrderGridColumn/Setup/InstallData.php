90b66351eeb7f23959ea622c6125bd5c3569cf03
sasha <alexander.pop.izmail@gmail.com>
2019-11-13 11:37:31 -0600

    Add data to quote table

--- OrderGridColumn/Setup/InstallData.php
+++ OrderGridColumn/Setup/InstallData.php
@@ -5,6 +5,7 @@ namespace Codelegacy\OrderGridColumn\Setup;
 
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Sales\Setup\SalesSetupFactory;
+use Magento\Quote\Setup\QuoteSetupFactory;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\DB\Ddl\Table;
@@ -16,10 +17,17 @@ class InstallData implements InstallDataInterface
      */
     private $salesSetupFactory;
 
+    /**
+     * @var QuoteSetupFactory
+     */
+    private $quoteSetupFactory
+
     public function __construct(
-        SalesSetupFactory $salesSetupFactory
+        SalesSetupFactory $salesSetupFactory,
+        QuoteSetupFactory $quoteSetupFactory
     ) {
         $this->salesSetupFactory = $salesSetupFactory;
+        $this->quoteSetupFactory = $quoteSetupFactory;
 
     }
 
@@ -29,8 +37,10 @@ class InstallData implements InstallDataInterface
      * @param ModuleContextInterface $context
      * @return void
      */
-    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
-    {
+    public function install(
+        ModuleDataSetupInterface $setup, 
+        ModuleContextInterface $context
+    ) {
         $setup->startSetup();
 
         /** @var \Magento\Sales\Setup\SalesSetup $salesInstaller */
@@ -41,15 +51,47 @@ class InstallData implements InstallDataInterface
             ]
         );
 
+        $quoteInstaller = $this->quoteSetupFactory->create(
+            [
+                'resourceName' => 'quote_setup', 
+                'setup' => $setup
+            ]
+        );
+
+        $quoteInstaller
+            ->addAttribute(
+                // quote, quote_item, quote_address, quote_address_item, quote_address_rate and quote_payment
+                'quote', 
+                'erp_code', 
+                [
+                    'type' => Table::TYPE_TEXT, 
+                    'length'=> 255, 
+                    'visible' => false, 
+                    'nullable' => true
+                ]
+            )->addAttribute(
+                'quote', 
+                'erp_status', 
+                [
+                    'type' => Table::TYPE_TEXT, 
+                    'length'=> 255, 
+                    'visible' => false, 
+                    'nullable' => true
+                ]
+            );
+
         $salesInstaller
             ->addAttribute(
+                // order, order_payment, order_item, order_address, order_status_history, invoice, invoice_item, etc.
+                // see Magento/Sales/Setup/SalesSetup.php
                 'order', 
                 'erp_code', 
                 [
                     'type' => Table::TYPE_TEXT,
                     'length'=> 255,
                     'visible' => false,
-                    'nullable' => true
+                    'nullable' => true,
+                    'grid' => true
                 ]
             )->addAttribute(
                 'order', 
@@ -58,7 +100,8 @@ class InstallData implements InstallDataInterface
                     'type' => Table::TYPE_TEXT,
                     'length'=> 255,
                     'visible' => false,
-                    'nullable' => true
+                    'nullable' => true,
+                    'grid' => true
                 ]
             );
 
